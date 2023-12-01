9b30da2e423944c87d8c45295a25690aa57ed90a
UnknownName114514 <2010446010@qq.com>
2023-12-01 18:50:31 +0800

    在学了一段时间的编程后，内心的想法出现了变化

--- hello.c
+++ hello.c
@@ -4,12 +4,6 @@
 int main(){
 
 	printf("hello,world!\n");
-	printf("hello,world!\n");
-	printf("hello,world!\n");
-	printf("hello,world!\n");
-	printf("hello,world!\n");
-	printf("hello,world!\n");
-	
-
+	printf("好累啊，毁灭吧！");
 	return 0;
 }
